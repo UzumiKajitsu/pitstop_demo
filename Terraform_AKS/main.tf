f71a6e40d2676ac3cb8b43edfa49f6f7bba5ee0f
UzumiKajitsu <minh75606@gmail.com>
2025-05-21 09:36:00 +0700

    fix az

--- Terraform_AKS/main.tf
+++ Terraform_AKS/main.tf
@@ -1,8 +1,6 @@
 provider "azurerm" {
   features {}
-
-  subscription_id = "c8145831-11d0-49fd-9aa1-06c8cd232667"
-  resource_provider_registrations = "none"
+  use_cli = true
 }
 
 resource "azurerm_resource_group" "rg" {
