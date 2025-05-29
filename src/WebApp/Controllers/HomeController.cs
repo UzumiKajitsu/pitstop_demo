ca5f73a2b7670df1a79698d2ae5ba9df1536fc0d
UzumiKajitsu <minh75606@gmail.com>
2025-05-29 13:39:21 +0700

    add http HEAD for homecontroller.cs

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -3,18 +3,21 @@
 public class HomeController : Controller
 {
     [HttpGet]
+    [HttpHead]
     public IActionResult Index()
     {
         return View();
     }
 
     [HttpGet]
+    [HttpHead]
     public IActionResult About()
     {
         return View();
     }
 
     [HttpGet]
+    [HttpHead]
     public IActionResult Error()
     {
         return View();
